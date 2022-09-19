8f94e5c666b6c3b953cf0124c41359a47fcc8612
Ganzay <muhammadganakabiru@gmail.com>
2022-09-19 15:55:57 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,6 +3,9 @@
 /**
  * _puts - prints a string to stdout
  * @str: pointer to the string to print
+ *
+ * Desscription: prints a string
+ * On success: returns no error
  */
 
 void _puts(char *str)
