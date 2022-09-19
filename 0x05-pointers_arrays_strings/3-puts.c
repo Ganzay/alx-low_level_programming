914f1c1cf0b76f0accd48f72e1d24d70b2e3b38c
Ganzay <muhammadganakabiru@gmail.com>
2022-09-19 15:45:48 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -4,13 +4,15 @@
  * _puts - prints a string to stdout
  * @str: pointer to the string to print
  */
+
 void _puts(char *str)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; str[i] != '\0'; i++)
+	while (*(str + i) != '\0')
 	{
-		_putchar(str[i]);
+		putchar(*(str + i));
+		i++;
 	}
-	_putchar('\n');
+	putchar(10);
 }
